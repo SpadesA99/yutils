710cd16e4bf1b0fdbca805e7360695d5d076f031
SPADESA\56414 <yanjuan9998@qq.com>
2021-11-23 14:25:49 +0800

    no message

--- yutils/YFileUtils.cpp
+++ yutils/YFileUtils.cpp
@@ -1,6 +1,31 @@
 #include "YFileUtils.h"
 #include <windows.h>
 
+bool YFileUtils::ForceDelFile(std::string path)
+{
+	char temp_path[MAX_PATH]{ 0 }, target_name[MAX_PATH]{ 0 };
+
+	GetTempPathA(MAX_PATH, temp_path);
+	GetTempFileNameA(temp_path, "edgo", 0, target_name);
+
+	if (!MoveFileExA(path.c_str(), target_name, MOVEFILE_REPLACE_EXISTING | MOVEFILE_COPY_ALLOWED))
+	{
+		return false;
+	}
+	if (!MoveFileExA(target_name, NULL, MOVEFILE_DELAY_UNTIL_REBOOT))
+	{
+		return false;
+	}
+	return true;
+}
+
+std::string YFileUtils::get_current_dir()
+{
+	std::string path(MAX_PATH, 0);
+	GetModuleFileNameA(nullptr, path.data(), MAX_PATH);
+	return path.substr(0, path.rfind("\\"));
+}
+
 void YFileUtils::YMenuFile(std::string path, std::function<bool(std::string)> dir_callback, std::function<bool(std::string)>file_callback)
 {
 	WIN32_FIND_DATAA pNextInfo;
--- yutils/YFileUtils.h
+++ yutils/YFileUtils.h
@@ -4,6 +4,15 @@
 
 class YFileUtils
 {
-	void YMenuFile(std::string path, std::function<bool(std::string)> dir_callback, std::function<bool(std::string)>file_callback);
+
+public:
+	YFileUtils() {};
+	~YFileUtils() {};
+public:
+	static bool ForceDelFile(std::string path);
+
+	static std::string get_current_dir();
+
+	static void YMenuFile(std::string path, std::function<bool(std::string)> dir_callback, std::function<bool(std::string)>file_callback);
 };
 
